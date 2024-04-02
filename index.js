c7dfe7273f8cd0f37244e8d5af0498eb54b89b39
varun__upadhyay <varun.802.vu@gmail.com>
2024-04-02 12:38:42 +0530

    short id shown  in the home page via test route

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 const express = require("express")
 const { connectToMongoDB } = require("./connect")
+const path = require("path")
 const urlRoute = require("./routes/url")
+const URL = require("./models/url")
 const app = express();
 const PORT = 8000;
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() => console.log("connected to mongoDB"))
+
+app.set("view engine", "ejs");
+//we have used path which is a built in module to redirect to view folder
+//we have successfully rendered home from views
+app.set('views', path.resolve("./views"))
+
 app.use(express.json())
+
 app.use("/url", urlRoute)
+
+app.get("/test", async (req, res) => {
+    try {
+        const allUrls = await URL.find({})
+        // Render the 'home' template with the fetched URLs
+        res.render('home', {
+            urls: allUrls
+        })
+    } catch (error) {
+        // Handle errors appropriately
+        console.error("Error fetching URLs:", error)
+        res.status(500).send("Internal Server Error")
+    }
+})
 
 app.listen(PORT, () => { console.log(`server started at PORT ${PORT}`) })
